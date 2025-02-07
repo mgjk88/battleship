4724e0c3567bdadf0bd40bf1fdf5ebfcaaf7e895
mgjk88 <159707804+mgjk88@users.noreply.github.com>
2025-02-07 15:43:25 +0800

    feat: add game logic
    create ship, gameboard and player classes.

---
+++ src/logic.js
@@ -0,0 +1,99 @@
+class Ship {
+  constructor(length = 1) {
+    this.length = length;
+    this.hits = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    if (this.hits < this.length) {
+      this.hits += 1;
+    }
+  }
+
+  isSunk() {
+    return this.hits >= this.length;
+  }
+}
+
+class Gameboard {
+  constructor() {
+    this.ships = [];
+    this.board = new Array(10).fill(null).map(() => new Array(10).fill(null));
+    this.miss = [];
+  }
+
+  static #inBoard(x, y) {
+    return x >= 0 && x < 10 && y >= 0 && y < 10;
+  }
+
+  place(x1, y1, x2, y2) {
+    const isValid = (x1, y1, x2, y2) => {
+      if (x1 !== x2 && y1 !== y2) throw new Error("Invalid Orientation!");
+
+      for (let arg of arguments) {
+        if (
+          typeof arg !== "number" ||
+          !Gameboard.#inBoard(x1, y1) ||
+          !Gameboard.#inBoard(x2, y2)
+        )
+          throw new Error("Invalid Coordinates!");
+      }
+
+      for (let x = x1; x <= x2; x += 1) {
+        for (let y = y1; y <= y2; y += 1) {
+          if (this.board[y][x] !== null)
+            throw new Error("Coordinates conflict with existing ship!");
+        }
+      }
+    };
+
+    if (x1 > x2) [x1, x2] = [x2, x1];
+    if (y1 > y2) [y1, y2] = [y2, y1];
+
+    isValid(x1, y1, x2, y2);
+    let constAxis = x1 === x2 ? "x" : "y";
+    let len = x2 - x1 + y2 - y1 + 1;
+    //at least one pair will cancel themselves out
+    this.ships.push(new Ship(len));
+    let idx = this.ships.length - 1;
+
+    if (constAxis === "x") {
+      for (let y = y1; y <= y2; y += 1) {
+        this.board[y][x1] = idx;
+      }
+    } else {
+      for (let x = x1; x <= x2; x += 1) {
+        this.board[y1][x] = idx;
+      }
+    }
+  }
+
+  receiveAttack(x, y) {
+    const isValid = (x, y) => {
+      return typeof(x) === 'number' && typeof(y) === 'number' && Gameboard.#inBoard(x, y)
+    };
+    if(!isValid(x,y)) throw new Error("Invalid Coordinates!");
+    if(this.board[y][x] === null) {
+      this.miss.push([x,y])
+      return;
+    };
+    this.ships[this.board[y][x]].hit();
+  }
+
+  allSunk(){
+    for(let ship of this.ships){
+      if(ship.isSunk() === false) return false;
+    }
+    return true;
+  }
+}
+
+class Player{
+  constructor(type){
+    this.type = type;
+    this.board = new Gameboard();
+  }
+}
+
+export { Ship, Gameboard, Player };
---
+++ tests/logic.test.js
@@ -0,0 +1,60 @@
+import { Ship, Gameboard} from "../src/logic.js";
+
+describe("class ship", () => {
+  test(".hit() incr ship.hits", () => {
+    let ship = new Ship(1);
+    expect(ship.hits).toBe(0);
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  test(".isSunk() is true when ship.hits >= ship.length ", ()=>{
+    let ship = new Ship(1);
+    expect(ship.isSunk()).toBeFalsy();
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy();
+  });
+});
+
+describe.only("class Gameboard", () => {
+    let board = new Gameboard();
+    test(".place(x1,y1,x2,y2) record ship placement",()=>{
+        board.place(0,0,0,0);
+        expect(board.ships.length).toBe(1);
+    });
+
+    test(".place(x1,y1,x2,y2) record ship correct ship length",()=>{
+        expect(board.ships[0].length).toBe(1);
+    });
+
+    test(".place(x1,y1,x2,y2) detects invalid ship orientation", () => {
+      expect(() => board.place(1,2,3,4)).toThrow("Invalid Orientation!");
+    })
+
+    test(".place(x1,y1,x2,y2) detects ship overlap",()=>{
+      board.place(1,2,1,6);
+      expect(() => board.place(0,0,5,0)).toThrow("Coordinates conflict with existing ship!");
+    })
+
+    test(".receiveAttack(x, y) records miss",()=>{
+      board.receiveAttack(1,1);
+      expect(board.miss[0]).toEqual([1,1]);
+    });
+
+    test(".receiveAttack(x, y) records hit",()=>{
+      board.receiveAttack(0,0);
+      expect(board.ships[board.board[0][0]].isSunk()).toBeTruthy();
+    });
+
+    test(".allSunk detects not all ships are sunk", () => {
+      board = new Gameboard();
+      board.place(0,0,0,1);
+      expect(board.allSunk()).toBeFalsy();
+    });
+
+    test(".allSunk detects all ships are sunk", () => {
+      board.receiveAttack(0,0);
+      board.receiveAttack(0,1);
+      expect(board.allSunk()).toBeTruthy();
+    });
+});
